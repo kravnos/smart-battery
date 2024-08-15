193e62ce70c10fe2be12bf394d7a1055d734cea6
Thomas Van Veen <156453682+kravnos@users.noreply.github.com>
2024-08-15 13:18:52 -0400

    Add files via upload

--- battery.bat
+++ battery.bat
@@ -3,12 +3,26 @@
 SET "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
+SET "TIMEOUT="
+SET "IDLE=900"
+SET "SLEEP=0"
 SET "date=%date: =-%"
 SET "date=%date:/=-%"
 SET "LOGFILE=logs\battery-%date%.log"
 
 FOR /F "tokens=2 delims=:" %%A IN ('powercfg /query SCHEME_CURRENT SUB_SLEEP STANDBYIDLE ^| findstr /C:"Current DC Power Setting Index:"') DO (
-	SET /A "TIMEOUT=%%A-900"
+	SET /A "TIMEOUT=%%A-%IDLE%"
+)
+
+IF "%TIMEOUT%"=="" (
+	SET "TIMEOUT=0"
+)
+
+IF %TIMEOUT% LEQ 0 (
+    FOR /F "tokens=2 delims=:" %%B IN ('powercfg /query SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE ^| findstr /C:"Current DC Power Setting Index:"') DO (
+        SET /A "TIMEOUT=%%B-%IDLE%"
+	SET "SLEEP=1"
+    )
 )
 
 IF NOT "%params%"=="" (
@@ -19,7 +33,7 @@ IF NOT "%params%"=="" (
 			)
 		)
 	)
-	GOTO SKIP
+	GOTO NOLOG
 )
 
 IF NOT EXIST "logs\" (
@@ -29,8 +43,7 @@ IF NOT EXIST "logs\" (
 CALL :LOG >> %LOGFILE%
 EXIT /B
 :LOG
-
-:SKIP
+:NOLOG
 
 "%WINDIR%\SYSTEM32\WindowsPowerShell\v1.0\powershell.exe" -executionpolicy bypass -file "battery.ps1" %params%
 
@@ -48,7 +61,7 @@ IF NOT "%params%"=="" (
 	IF "%params%"=="sleep" (
 		IF NOT "%TIMEOUT%"=="" (
 			IF %TIMEOUT% GTR 0 (
-				rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+				rundll32.exe powrprof.dll,SetSuspendState %SLEEP%,1,%SLEEP%
 			)
 		)
 	)
