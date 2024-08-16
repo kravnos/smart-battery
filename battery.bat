fd291b7d054aaaf9c9ab676dc6ca40ebd7c65938
Thomas Van Veen <156453682+kravnos@users.noreply.github.com>
2024-08-16 12:34:07 -0400

    Add files via upload

--- battery.bat
+++ battery.bat
@@ -5,28 +5,51 @@ SET "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || ( echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 :: Initialize variables
-SET "TIMEOUT="  :: Clear TIMEOUT variable
-SET "IDLE=900"  :: Time in seconds for task scheduler idle check
-SET "SLEEP=0"   :: Determines whether to sleep or hibernate
+SET "TIMEOUT="    :: Clear TIMEOUT variable
+SET "SUSPEND="    :: Determines whether to sleep or hibernate
+SET "SLEEP=0"     :: Determines if sleep is available
+SET "HIBERNATE=0" :: Determines if hibernate is available
+SET "IDLE=900"    :: Time in seconds for task scheduler idle check
 SET "date=%date: =-%"
 SET "date=%date:/=-%"
 SET "LOGFILE=logs\battery-%date%.log"
 
-:: Calculate the timeout based on the current power settings
-FOR /F "tokens=2 delims=:" %%A IN ('powercfg /query SCHEME_CURRENT SUB_SLEEP STANDBYIDLE ^| findstr /C:"Current DC Power Setting Index:"') DO (
-    SET /A "TIMEOUT=%%A-%IDLE%"
-)
+:: Check available sleep states
+FOR /F "tokens=*" %%A IN ('powercfg /AVAILABLESLEEPSTATES') DO (
+    IF "%%A"=="The following sleep states are not available on this system:" (
+        GOTO :BREAK
+    )
 
-:: If TIMEOUT is not set, default to 0
-IF "%TIMEOUT%"=="" (
-    SET "TIMEOUT=0"
-)
-
-:: Adjust timeout if hibernate settings are present
-IF %TIMEOUT% LEQ 0 (
-    FOR /F "tokens=2 delims=:" %%B IN ('powercfg /query SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE ^| findstr /C:"Current DC Power Setting Index:"') DO (
-        SET /A "TIMEOUT=%%B-%IDLE%"
+    IF "%%A"=="Standby (S3)" (
         SET "SLEEP=1"
+    )
+
+    IF "%%A"=="Hibernate" (
+        SET "HIBERNATE=1"
+    )
+)
+:BREAK
+
+:: Calculate the timeout based on the current power settings
+IF "%SLEEP%"=="1" (
+    FOR /F "tokens=2 delims=:" %%A IN ('powercfg /QUERY SCHEME_CURRENT SUB_SLEEP STANDBYIDLE ^| findstr /I /C:"Current DC Power Setting Index:"') DO (
+        SET /A "TIMEOUT=%%A-%IDLE%"
+        SET "SUSPEND=0"
+    )
+)
+
+IF "%HIBERNATE%"=="1" (
+    :: If TIMEOUT is not set, default to 0
+    IF "%TIMEOUT%"=="" (
+        SET "TIMEOUT=0"
+    )
+
+    :: Adjust timeout if hibernate settings are present
+    IF %TIMEOUT% LEQ 0 (
+        FOR /F "tokens=1 delims=:" %%B IN ('powercfg /QUERY SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE ^| findstr /I /C:"Current DC Power Setting Index:"') DO (
+            SET /A "TIMEOUT=%%B-%IDLE%"
+            SET "SUSPEND=1"
+        )
     )
 )
 
@@ -47,7 +70,7 @@ IF NOT EXIST "logs\" (
     MKDIR "logs\"
 )
 
-:: Log the session end
+:: Log the session
 CALL :LOG >> %LOGFILE%
 EXIT /B
 :LOG
@@ -57,22 +80,24 @@ EXIT /B
 "%WINDIR%\SYSTEM32\WindowsPowerShell\v1.0\powershell.exe" -executionpolicy bypass -file "battery.ps1" %params%
 
 :: Extract hours and minutes from the current time
-::FOR /F "TOKENS=1,2 DELIMS=:" %%A IN ("%TIME%") DO (
-::    SET "hh=%%A"
-::    SET "mm=%%B"
-::)
+:: FOR /F "TOKENS=1,2 DELIMS=:" %%A IN ("%TIME%") DO (
+::     SET "hh=%%A"
+::     SET "mm=%%B"
+:: )
 
 :: If parameters are provided and are "sleep", handle the sleep logic
 IF NOT "%params%"=="" (
-::	IF EXIST "%LOGFILE%" (
-::		"sendEmail.exe" -f email@domain.com -t email@domain.com -u [%date%] [%hh%:%mm%]: battery.bat -m Session End -s mail.domain.com:26 -xu email@domain.com -xp password -o tls=no -a "%LOGFILE%"
-::		TIMEOUT /T 2 /NOBREAK
-::	)
+::  IF EXIST "%LOGFILE%" (
+::          "sendEmail.exe" -f email@domain.com -t email@domain.com -u [%date%] [%hh%:%mm%]: battery.bat -m Session End -s mail.domain.com:26 -xu email@domain.com -xp password -o tls=no -a "%LOGFILE%"
+::      TIMEOUT /T 2 /NOBREAK
+::  )
 
     IF "%params%"=="sleep" (
         IF NOT "%TIMEOUT%"=="" (
             IF %TIMEOUT% GTR 0 (
-                rundll32.exe powrprof.dll,SetSuspendState %SLEEP%,1,%SLEEP%
+                IF NOT "%SUSPEND%"=="" (
+                    rundll32.exe powrprof.dll,SetSuspendState %SUSPEND%,1,%SUSPEND%
+                )
             )
         )
     )
